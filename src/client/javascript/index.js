1da470646f7dc2c920a2e94aed34eb89416c9d9a
kostas <eoppep@eoppep.gr>
2023-02-10 14:01:15 +0200

    bottom-left

--- src/client/javascript/index.js
+++ src/client/javascript/index.js
@@ -1,1 +1,16 @@
-console.log('test')
+const friendsButton = document.querySelector('#friendsButton')
+const groupsButton = document.querySelector('#groupsButton')
+const friendList = document.querySelector('#friendList')
+const groupList = document.querySelector('#groupList')
+
+
+
+friendsButton.addEventListener('click',e=>{
+    groupList.style.display = 'none'
+    friendList.style.display = 'block'
+})
+
+groupsButton.addEventListener('click',e=>{
+    friendList.style.display = 'none'
+    groupList.style.display = 'block'
+})
