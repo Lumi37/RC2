5f871ca29d36eb479d054e1759df5a9c8e9dbfbf
kostas <eoppep@eoppep.gr>
2023-03-03 17:28:12 +0200

    front end err fixed

--- src/client/javascript/modules/constructList.js
+++ src/client/javascript/modules/constructList.js
@@ -6,12 +6,14 @@ export function constructList(res){
     let list = res.userList
     let groups = res.rooms
     friendList.innerHTML   = ''
+    console.log('list eraser')
     groupList.innerHTML = ''
     list.forEach(user=>{ 
             if(user.id !== localStorage.id){
                 console.log(user.name,':',user.connectionStatus.status ==='online')
                 if(user.connectionStatus.status==='online'){
-                alert('mpainei sto online gia ton xristi: ',user.name,':',user.connectionStatus.status ==='online')
+                    console.log('IF pou shmainei oti user.connectionStatus.status==="online" TRUE!! ')
+                // alert('mpainei sto online gia ton xristi: ',user.name,':',user.connectionStatus.status ==='online')
                     friendList.innerHTML+=`
                     <li class="friendListItemsContainer" data-socketId='${user.socketId}' data-userLocalId='${user.id}'>  
                         <div id="imgContainerList">
@@ -38,16 +40,20 @@ export function constructList(res){
                         timeOffline =String(user.connectionStatus.offlineDifference.hours)+ 'h'
                     else
                         timeOffline =String(user.connectionStatus.offlineDifference.minutes)+ 'm'
-
+                    console.log('ELSE pou shmainei oti user.connectionStatus.status==="online" NOT TRUE \n',timeOffline,' <--')
                     friendList.innerHTML+=`
-                    <li class="friendListItemsContainer" data-id='${user.id}>  
+                    <li class="friendListItemsContainer" data-userLocalId='${user.id}'>  
                         <div id="imgContainerList">
                             <img src=${user.profilePicturePathname} id="listUserIcon" alt=""> 
                             <div id=${user.connectionStatus.status}></div>
                         </div>
                         <div class="listNameMessageContainer">
-                            <div id="userListName">${user.name}<div class="tooltip">Kostas</div></div>
-                            <div id="userLastMessage">${user.lastMessage.text}<div class="tooltip">>${user.lastMessage.text}</div></div>
+                            <div id="userListName">${user.name}
+                            <div class="tooltip">${user.name}</div>
+                            </div>
+                            <div id="userLastMessage">${user.lastMessage.text}
+                            <div class="tooltip">${user.lastMessage.text}</div>
+                            </div>
                         </div>
                         <div id="dateContainer"><div id="lastOnline">${timeOffline}</div></div>
                     </li>`
